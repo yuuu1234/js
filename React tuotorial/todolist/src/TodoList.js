de100aa950e18bd225961d8010163416866b7d4f
yu wang <718037359@qq.com>
2020-09-01 17:17:18 +0800

    add todolist

--- React tuotorial/todolist/src/TodoList.js
+++ React tuotorial/todolist/src/TodoList.js
@@ -1,57 +1,48 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputvalue : '',
-      list: [],
-    }
 
+  state = {
+    inputMsg: '',
+    messages: [],
+  };
+
+  handleInput = (evt) => {
+    this.setState({ inputMsg: evt.target.value });
   }
+
+  hadleSubmit = () => {
+    const text = this.state.inputMsg
+    if (text) {
+      this.setState({ messages: [...this.state.messages, text] });
+    };
+    this.setState({ inputMsg: '' });
+  }
+  // handleDelete = (ind)=>{
+  //   const oldMessages = [...this.state.messages]
+  //   this.setState({
+  //     messages: oldMessages.splice(ind,1),
+  //   });
+  // }
+
   render() {
     return (
-      <Fragment>
+      <div>
         <div>
-          <input
-            type="text"
-            value={this.state.inputvalue}
-            onChange={this.hadleInputChange.bind(this)}
-          >
-          </input>
-          <button>Submit</button>
+          <ul>
+            {this.state.messages.map((msg,ind) =>
+              <li>
+                <span>{msg}</span>
+                <span onClick={this.handleDelete(ind)}>X</span>
+              </li>)}
+          </ul>
         </div>
-        <ul>
-          <li>
-            <span>Eating</span>
-            <span>X</span>
-          </li>
-          <li>
-            <span>Sleeping</span>
-            <span>X</span>
-          </li>
-          <li>
-            <span>Playing</span>
-            <span>X</span>
-          </li>
-        </ul>
-      </Fragment>
+        <input type="text" onChange={this.handleInput} />
+        <button onClick={this.hadleSubmit}>submit</button>
+      </div>
     );
-
   }
-
-  hadleInputChange(e) {
-    console.log(e.target);
-    console.log(e.target.value);
-    this.setState({
-      inputvalue: e.target.value
-    })
-
-  };
 }
-
-
-
 export default TodoList;
